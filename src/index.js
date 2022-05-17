a7d7dfbbdac5ffbb58d09da18759a9a7d40d5801
John Tarus <47330228+johntarus@users.noreply.github.com>
2022-05-17 23:18:22 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+//object
+let stocks = {
+  Fruits: ["strawbery", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"]
+};
+
+let is_shop_open = true;
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("Our shop is closed"));
+    }
+  });
+};
+order(2000, () => console.log(`${stocks.Fruits[0]}  was selected`));
+
+document.getElementById("app").innerHTML = `
+
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
